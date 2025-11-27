11f13dd09733285e39992c34e57f4e51e47666ba
Pandurang Rane <pandurangrane16@gmail.com>
2025-11-27 14:45:37 +0530

    package and landing page animation

---
+++ src/app/components/cm-button/cm-button.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cm-button',
+  imports: [],
+  templateUrl: './cm-button.html',
+  styleUrl: './cm-button.css',
+})
+export class CmButton {
+  @Input() class :string = "bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 cursor-box pointer";
+  @Input() btnText : string = "Send Message";
+}
--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { FooterComponent } from "../../components/footers/footer/footer.component";
 import { CommonModule } from "@angular/common";
+import {FormsModule} from "@angular/forms";
 import { RouterLink } from "@angular/router";
 import { AuthNavbarComponent } from "../../components/navbars/auth-navbar/auth-navbar.component";
+import { CardTableComponent } from "../../components/cards/card-table/card-table.component";
+import { CmButton } from "../../components/cm-button/cm-button";
 
 @Component({
   selector: "app-landing",
   templateUrl: "./landing.component.html",
-  imports:[FooterComponent,CommonModule,RouterLink,AuthNavbarComponent],
+  imports: [FooterComponent, CommonModule, AuthNavbarComponent, CardTableComponent, CmButton,FormsModule],
   standalone:true
 })
 export class LandingComponent implements OnInit {
+  fullName : string ="";
+  description:string = "";
   constructor() {}
 
   ngOnInit(): void {}
