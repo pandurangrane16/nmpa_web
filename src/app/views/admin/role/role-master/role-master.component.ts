d784fa1b0c83b4d5e82367e06ade49fda57396fa
Pandurang Rane <pandurangrane16@gmail.com>
2025-12-28 18:15:15 +0530

    role master stepper

--- src/app/views/admin/role/role-master/role-master.component.ts
+++ src/app/views/admin/role/role-master/role-master.component.ts
@@ -4,10 +4,11 @@ import { CmTextarea } from '../../../../components/cm-textarea/cm-textarea';
 import { CmToggle } from '../../../../components/cm-toggle/cm-toggle';
 import { CmButton } from '../../../../components/cm-button/cm-button';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-role-master',
-  imports: [CmInput, CmTextarea, CmToggle, CmButton],
+  imports: [CmInput, CmTextarea, CmToggle, CmButton, CommonModule],
   templateUrl: './role-master.component.html',
   styleUrl: './role-master.component.css',
 })
@@ -20,5 +21,24 @@ export class RoleMasterComponent {
 
   backToList() {
     this.router.navigate(['admin/role-list']);
+  }
+
+
+  currentStep = 1;
+
+  next() {
+    if (this.currentStep < 3) {
+      this.currentStep++;
+    }
+  }
+
+  prev() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  goTo(step: number) {
+    this.currentStep = step;
   }
 }
